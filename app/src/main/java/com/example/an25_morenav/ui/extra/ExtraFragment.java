dc6cb11063ce1f0ad201cf7177bf021ba5339089
Susmita <susmitadutta@centersource.io>
2022-06-14 17:13:33 +0530

    Changes

--- app/src/main/java/com/example/an25_morenav/ui/extra/ExtraFragment.java
+++ app/src/main/java/com/example/an25_morenav/ui/extra/ExtraFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -32,6 +33,7 @@ public class ExtraFragment extends Fragment {
                 .placeholder(androidx.transition.R.drawable.abc_ic_star_black_16dp)
                 .into(binding.iv);
 
+        Toast.makeText(getContext(), "hello", Toast.LENGTH_SHORT).show();
         return root;
     }
 
